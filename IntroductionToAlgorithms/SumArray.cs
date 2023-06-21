e3b6a7a26fcb097a12b4104566ca35a1644d422e
atiyeh <atiyeh.85@gmail.com>
2023-06-20 20:56:11 -0600

    Add project files.

---
+++ IntroductionToAlgorithms/SumArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class SumArray
+    {
+
+        public static int Sum_Array(int[] A)
+        {
+            var sum = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                sum+= A[i]; 
+            }
+
+            return sum;
+        }
+    }
+}
