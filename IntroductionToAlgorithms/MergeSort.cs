229e908f333a37231ab8b05fb367c73e17ef54d5
atiyeh <atiyeh.85@gmail.com>
2023-06-22 16:40:56 -0600

    Add mergesort class.

---
+++ IntroductionToAlgorithms/MergeSort.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionToAlgorithms
+{
+    internal class MergeSort
+    {
+        public static List<int> merge(List<int> A, int p, int q, int r)
+        {
+
+            var nL = q - p + 1;// length of A[p:q]
+            var nR = r - q ;// length of A[q+1:r]
+            int[] L=new int[nL]; // L[0:nL-1]
+            int[] R=new int[nR]; // L[0:nL-1]
+            for (int i = 0; i < nL ; i++)
+            {
+                L[i]= A[p + i];
+            }
+            for (int j = 0; j < nR;  j++)
+            {
+                R[j] = A[q+ j + 1];
+
+            }
+            int c = 0;// c indexes the smallest remaining element in L
+            int d = 0;// d indexes the smallest remaining element in R
+            int k=p; //k indexes the location in A to fill
+            // As long as each of the arrays L and R contains an unmerged element,
+            // copy the smallest unmerged element back into A[p:r].
+            while (c<nL && d< nR){
+
+                if (L[c] < R[d])
+                {
+                    A[k] = L[c];
+                    c++;
+                }
+                 else
+                 {
+                    A[k] = R[d];
+                    d++;
+                 }
+            k++;
+            }
+            // Having gone through one of L and R entirely, copy the
+            // remainder of the other to the end of A[p:r].
+
+            while (c < nL )
+            {
+                A[k] = L[c];
+                c++;
+                k++;
+            }
+            while (d <  nR )
+            {
+                A[k] = R[d];
+                d++;
+                k++;
+            }
+            return A;
+        }
+        public static List<int> merge_sort(List<int> A, int p, int r)
+        {
+            if (p>=r)
+            {
+                return A;
+            }
+            var q = Math.Abs((p + r) / 2);
+            merge_sort(A, p, q);
+            
+            merge_sort(A,q+1, r);
+           var List= merge(A, p, q,r);
+            return List;
+        }
+    }
+}
--- IntroductionToAlgorithms/Program.cs
+++ IntroductionToAlgorithms/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IntroductionToAlgorithms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,12 +11,31 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter username:");
+            // Create a string variable and get user input from the keyboard and store it in the variable
+            string userName = Console.ReadLine();
+            // Print the value of the variable (userName), which will display the input value
+            Console.WriteLine("Username is: " + userName);
             var watch = new System.Diagnostics.Stopwatch();
             watch.Start();
-            int[] A = { 1,1,1 };
-            int[] B = { 1 };
+            // Mergsort
+            int p, q, r;
+            r = 8;
+            List<int> myList = new List<int>();
+            Random rnd = new Random();
 
-            var ss = AddBinaryInteger.ADD_BINARY_INTEGERS(A, B);
+            for (int i = 0; i < 100; ++i)
+            {
+                int rd = rnd.Next(1, 20);
+                myList.Add(rd);
+            }
+            int len = rnd.Next(1, 20);
+            //List<int> A = myList.GetRange(50, len);
+            List<int> A = new List<int>() { 12, 3, 7, 9, 14, 6, 11, 2 };
+            p = 0;
+            r = A.Count() - 1;
+            q = p + (r - p) / 2;
+            var s = MergeSort.merge_sort(A, p, r);
             watch.Stop();
             Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
         }
