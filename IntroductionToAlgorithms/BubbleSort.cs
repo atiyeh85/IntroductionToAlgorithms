d378e6610f742a143cb036c0c75b5f492f0f7031
atiyeh <atiyeh.85@gmail.com>
2023-06-23 21:25:18 -0600

    Bubble sort is updated.

---
+++ IntroductionToAlgorithms/BubbleSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionToAlgorithms
+{
+    internal class BubbleSort
+    {
+        public static List<int> Bubble_Sort(List<int> A, int n)
+        {
+            var temp = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = n; j >= i + 1; j--)
+                {
+                    if (A[j] < A[j - 1])
+                    {
+                        temp = A[j - 1];
+                        A[j - 1] = A[j];
+                        A[j] = temp;
+                    }
+
+                }
+            }
+
+            return A;
+        }
+    }
+}
